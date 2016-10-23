0069d2c66791ed56a7ee5e7eaf1c01c6dc848d0c
Ece Kubilay <ecekubilay93@gmail.com>
2016-10-23 22:29:18 +0200

    Added now

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -8,11 +10,18 @@
  */
 public class HelloWorld {
 
+	private static Scanner a;
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello ASE2016 - how are you? :)");
+		 System.out.println("Hello ASE2016 - how are you now? :)");
+		 a = new Scanner(System.in);
+		 System.out.print("Please enter your name :");
+		 String s=a.nextLine();
+		 HelloUser.greetUser(s);
 	}
 
 }
+
